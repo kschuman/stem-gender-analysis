68a10a75428f31a122fffe35deb0326c8788c805
Kira Schuman <kira.schuman@gmail.com>
2017-12-10 18:40:17 -0500

    Update

--- Code/clean-career-goals.py
+++ Code/clean-career-goals.py
@@ -30,9 +30,14 @@ df = df.merge(careers, how='left', left_on='value', right_on='career')
 df = df.drop(['Unnamed: 0', 'career'], axis=1)
 df.columns = ['id', 'gender', 'cohort', 'yr', 'career', 'stem', 'medicine','social science']
 
+# Only Fall responses
+df = df[df['yr'].isin(['F7', 'F8', 'F9', 'F10', 'F11', 'F12'])]
+
 # Set time interval
-dict = {'F7':1, 'S7':2, 'F8':3, 'S8':4, 'F9':5, 'S9':6, 'F10':7, 'S10':8, 'F11':9, 'S11':10, 'F12':11, 'S12':12}
+#dict = {'F7':1, 'S7':2, 'F8':3, 'S8':4, 'F9':5, 'S9':6, 'F10':7, 'S10':8, 'F11':9, 'S11':10, 'F12':11, 'S12':12}
+dict = {'F7':0, 'F8':1, 'F9':2, 'F10':3, 'F11':4, 'F12':5}
 df['time'] = df['yr'].map(dict)
+
 
 # Save csv
 df.to_csv('stemm_career_goal.csv')
