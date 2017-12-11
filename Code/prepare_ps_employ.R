42a0b4112ccc6bb9220fc88e9c00f5647193e156
Meredith <meredith.miller10@gmail.com>
2017-12-10 19:48:22 -0500

    Added code to get data into long format. Also added a CSV to translate the survey year to the year from 7th grade for each cohort.

--- Code/prepare_ps_employ.R
+++ Code/prepare_ps_employ.R
@@ -6,17 +6,19 @@ load("Data/30263-0001-Data.rda")
 rawData <- da30263.0001
 
 # Recode majors
-NA10 <- levels(allData$NA10)
-OC12 <- levels(allData$OC12)
-QC10 <- levels(allData$QC10)
+NA10 <- levels(rawData$NA10)
+OC12 <- levels(rawData$OC12)
+QC10 <- levels(rawData$QC10)
 majors <- data.frame(survey=c(rep('P1',length(NA10)),
                               rep('P2',length(OC12)),
                               rep('P4',length(QC10))),
                      major=c(NA10,OC12,QC10))
-write.table(major,"Data/majors.csv",row.names = FALSE,sep=',')
+write.table(majors,"Data/majors.csv",row.names = FALSE,sep=',')
 
 # Pull out relevant columns
-col_meta <- c("CASENUM","COHORT","GENDER")
+
+# Information
+col_meta <- c("CASENUM","COHORT","GENDER","STRATA")
 col_ps <- c("NA2","OC2","QC2",
             "NA10","OC12","QC10",
             "WEIGHTP1","WEIGHTP2","WEIGHTP4")
@@ -60,9 +62,40 @@ cleanData <- allData %>%
          j09_stem=ifelse(is.na(TSTEMM10),NA,as.numeric(TSTEMM10) %in% stem_jobs),
          j10_stem=ifelse(is.na(USTEMM10),NA,as.numeric(USTEMM10) %in% stem_jobs),
          j11_stem=ifelse(is.na(VSTEMM10),NA,as.numeric(VSTEMM10) %in% stem_jobs)) %>%
-  mutate(j07_stem=ifelse(j07_stem==TRUE,1,ifelse(is.na(j07_stem),NA,0)),
-         j08_stem=ifelse(j08_stem==TRUE,1,ifelse(is.na(j08_stem),NA,0)),
-         j09_stem=ifelse(j09_stem==TRUE,1,ifelse(is.na(j09_stem),NA,0)),
-         j10_stem=ifelse(j10_stem==TRUE,1,ifelse(is.na(j10_stem),NA,0)),
-         j11_stem=ifelse(j11_stem==TRUE,1,ifelse(is.na(j11_stem),NA,0)))
+  mutate(j07_stem=ifelse(j07_stem,1,ifelse(is.na(j07_stem),NA,0)),
+         j08_stem=ifelse(j08_stem,1,ifelse(is.na(j08_stem),NA,0)),
+         j09_stem=ifelse(j09_stem,1,ifelse(is.na(j09_stem),NA,0)),
+         j10_stem=ifelse(j10_stem,1,ifelse(is.na(j10_stem),NA,0)),
+         j11_stem=ifelse(j11_stem,1,ifelse(is.na(j11_stem),NA,0)))
 glimpse(cleanData)
+
+# Convert to long format
+years_translator <- read.csv("Data/years_translator.csv",header=TRUE)
+cohort_years <- years_translator %>%
+  gather(key="cohort",value="life_stage",cohort2,cohort1) %>%
+  separate(cohort,into=c("junk","cohort"),sep=-2) %>%
+  select(-junk) %>%
+  mutate(cohort=as.numeric(cohort))
+
+longData <- cleanData %>%
+  select(CASENUM,COHORT,GENDER,STRATA,
+         WEIGHTP1,WEIGHTP2,WEIGHTP4,
+         WEIGHTR,WEIGHTS,WEIGHTT,WEIGHTU,WEIGHTV,
+         p1_stem,p2_stem,p4_stem,
+         j07_stem,j08_stem,j09_stem,
+         j10_stem,j11_stem) %>%
+  unite(p1,WEIGHTP1,p1_stem) %>%
+  unite(p2,WEIGHTP2,p2_stem) %>%
+  unite(p4,WEIGHTP4,p4_stem) %>%
+  unite(j07,WEIGHTR,j07_stem) %>%
+  unite(j08,WEIGHTS,j08_stem) %>%
+  unite(j09,WEIGHTT,j09_stem) %>%
+  unite(j10,WEIGHTU,j10_stem) %>%
+  unite(j11,WEIGHTV,j11_stem) %>%
+  gather(key="life_stage",value="values",
+         p1,p2,p4,j07,j08,j09,j10,j11) %>%
+  separate(values,into=c("weight","engagement"),sep="_") %>%
+  mutate_at(vars(weight,engagement),funs(as.numeric)) %>%
+  mutate(cohort = as.numeric(COHORT)) %>%
+  left_join(cohort_years,by=c("cohort"="cohort","life_stage"="life_stage"))
+
