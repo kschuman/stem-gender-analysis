1b32bc4a6e87e040a9b551d151ccbcff9c4b41c6
Kira Schuman <kira.schuman@gmail.com>
2017-12-10 20:31:46 -0500

    Update

--- Code/career-goal-analysis.R
+++ Code/career-goal-analysis.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(geepack)
 library(dplyr)
 
-
+# Read data and merge to full dataset
 setwd('stem-gender-analysis')
 cgoal <- read.csv('Data/career-goals.csv')
 cgoal <- cgoal[c('CASENUM', 'COHORT', 'GENDER', 'STRATA', 'stem', 'weight', 'time')]
@@ -13,26 +13,20 @@ after_hs <- after_hs[c('CASENUM', 'COHORT', 'GENDER', 'STRATA', 'engagement', 'w
 names(cgoal) <- names(after_hs)
 
 all_data <- rbind(cgoal, after_hs)
-glimpse(all_data)
-
 
 # Get means
 means <- aggregate(engagement ~ GENDER + year_from_baseline, data = all_data, FUN=mean, na.rm = TRUE, weights=weight)
 means
 
-
 # Plot means
 ggplot(means, aes(x=as.numeric(year_from_baseline), y=engagement, group=GENDER, color=GENDER)) + 
   geom_point() +geom_line()
 
-
-# GEE model
+# GEE model - overall
 m1 <- geeglm(engagement ~ GENDER*year_from_baseline, data = cgoal, id=CASENUM, family=binomial(), weights=weight)
 summary(m1)
 
-glimpse(cgoal)
 
-View(all_data)
 
 # Grade school
 grade <- all_data[which(all_data$year_from_baseline < 6),]
